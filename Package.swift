cdd79e61de8ac3fcd104f30bd6927023f700fac6
Eugene Mozharovsky <mozharovsky@live.com>
2017-08-13 20:01:18 +0300

    Using local `fluent` repo

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     	// Robust MySQL interface for Swift.
    		.Package(url: "https://github.com/vapor/mysql.git", majorVersion: 2),
    		// Swift models, relationships, and querying for NoSQL and SQL databases.
-   		.Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2),
+   		.Package(url: "https://github.com/mozharovsky/fluent.git", majorVersion: 2),
    		// Random number generation
    		.Package(url: "https://github.com/vapor/random.git", majorVersion: 1),
     ]
